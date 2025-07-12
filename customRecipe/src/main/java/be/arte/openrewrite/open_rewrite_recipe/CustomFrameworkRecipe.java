28879649677a474f0c62018b7e85403006f0fba6
julien Arte <julien.arte@sfpd.fgov.be>
2025-07-12 14:35:00 +0200

    fix setup test

--- customRecipe/src/main/java/be/arte/openrewrite/open_rewrite_recipe/CustomFrameworkRecipe.java
+++ customRecipe/src/main/java/be/arte/openrewrite/open_rewrite_recipe/CustomFrameworkRecipe.java
@@ -5,41 +5,32 @@ import org.openrewrite.NlsRewrite;
 import org.openrewrite.Recipe;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.JavaTemplate;
-import org.openrewrite.java.search.UsesType;
 import org.openrewrite.java.tree.J;
 
-import java.util.Collections;
-
-public class CustomFrameworkRecipe extends Recipe
-{
+public class CustomFrameworkRecipe extends Recipe {
 
 	@Override
-	public @NlsRewrite.DisplayName String getDisplayName()
-	{
+	public @NlsRewrite.DisplayName String getDisplayName() {
 		return "update usage of custom framework to new API ";
 	}
 
 	@Override
-	public @NlsRewrite.Description String getDescription()
-	{
+	public @NlsRewrite.Description String getDescription() {
 		return "remove usage of old framweork to use the new API instead .";
 	}
 
 	@Override
-	public JavaIsoVisitor<ExecutionContext> getVisitor()
-	{
+	public JavaIsoVisitor<ExecutionContext> getVisitor() {
 		return new CustomFrameworkRecipe.AnnotationApplicationScopedAddVisitor();
 	}
 
-	private static class AnnotationApplicationScopedAddVisitor extends JavaIsoVisitor<ExecutionContext>
-	{
+	private static class AnnotationApplicationScopedAddVisitor extends JavaIsoVisitor<ExecutionContext> {
+
 
 		@Override
-		public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx)
-		{
-			if (classDecl.getBody() != null)
-			{
-				if(migrationDetected(classDecl, ctx)){
+		public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
+			if (classDecl.getBody() != null) {
+				if (isUsingLegacyFramework(classDecl, ctx)) {
 					classDecl = appliMigration(classDecl, ctx);
 				}
 			}
@@ -48,37 +39,39 @@ public class CustomFrameworkRecipe extends Recipe
 
 		private J.ClassDeclaration appliMigration(J.ClassDeclaration classDecl, ExecutionContext ctx) {
 
-			// Remove old imports
-			maybeRemoveImport("be.arte.openrewrite.open_rewrite_recipe.class_exemple.HibernateMetaModelCache");
-			maybeRemoveImport("be.arte.openrewrite.open_rewrite_recipe.class_exemple.IntegratedDossier");
 			return (J.ClassDeclaration) new UpdateClassVisitor().visit(classDecl, ctx);
 		}
 
-		private boolean migrationDetected(J.ClassDeclaration classDecl, ExecutionContext ctx) {
-			return new UsesType<>("be.arte.openrewrite.open_rewrite_recipe.class_exemple.Person", false).visit(classDecl, ctx) != classDecl;
+		private boolean isUsingLegacyFramework(J.ClassDeclaration classDecl, ExecutionContext ctx) {
+			J.CompilationUnit cu = getCursor().firstEnclosing(J.CompilationUnit.class);
+			return cu.getImports().stream()
+					.anyMatch(imp -> {
+						String importName = imp.getQualid().toString();
+						return importName.equals("be.arte.openrewrite.open_rewrite_recipe.class_exemple.HibernateMetaModelCache");
+					});
 		}
 
+}
+
+private static class UpdateClassVisitor extends JavaIsoVisitor<ExecutionContext> {
+	private final JavaTemplate template = JavaTemplate.builder("@Inject private SignaleticApi signaleticApi;").build();
+
+	@Override
+	public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext executionContext) {
+		classDecl = (J.ClassDeclaration) super.visitClassDeclaration(classDecl, executionContext);
+		classDecl = classDecl.withBody(classDecl.getBody().withStatements(template.apply(getCursor(), classDecl.getBody().getCoordinates().firstStatement())));
+		return classDecl;
 	}
 
-	private static class UpdateClassVisitor extends JavaIsoVisitor<ExecutionContext> {
-		private final JavaTemplate template = JavaTemplate.builder("@Inject private SignaleticApi signaleticApi;").build();
-
-		@Override
-		public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext executionContext) {
-			classDecl = (J.ClassDeclaration) super.visitClassDeclaration(classDecl, executionContext);
-			classDecl = classDecl.withBody(classDecl.getBody().withStatements(template.apply(getCursor(), classDecl.getBody().getCoordinates().firstStatement())));
-			return classDecl;
+	@Override
+	public J.MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext executionContext) {
+		method = super.visitMethodInvocation(method, executionContext);
+		if (method.getMethodType() != null && method.getMethodType().getName().equals("getModelRoot")) {
+			JavaTemplate template = JavaTemplate.builder("signaleticApi.getSignaletic(#{any(long)})").build();
+			return template.apply(getCursor(), method.getCoordinates().replace(), method.getArguments().get(1));
 		}
-
-		@Override
-		public J.MethodInvocation visitMethodInvocation(J.MethodInvocation method, ExecutionContext executionContext) {
-			method = super.visitMethodInvocation(method, executionContext);
-			if (method.getMethodType() != null && method.getMethodType().getName().equals("getModelRoot")) {
-				JavaTemplate template = JavaTemplate.builder("signaleticApi.getSignaletic(#{any(long)})").build();
-				return template.apply(getCursor(), method.getCoordinates().replace(), method.getArguments().get(1));
-			}
-			return method;
-		}
+		return method;
 	}
+}
 }
 
--- customRecipe/src/test/java/be/arte/openrewrite/open_rewrite_recipe/CustomFrameworkTest.java
+++ customRecipe/src/test/java/be/arte/openrewrite/open_rewrite_recipe/CustomFrameworkTest.java
@@ -2,14 +2,10 @@ package be.arte.openrewrite.open_rewrite_recipe;
 
 import static org.openrewrite.java.Assertions.java;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 import org.openrewrite.test.RecipeSpec;
